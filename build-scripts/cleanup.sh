8c2cfb89876f2005696e98fe4f76771fe56cd5fb
Ricardo Catalinas Jiménez <r@untroubled.be>
2022-03-28 11:49:08 +0100

    Fixes

--- build-scripts/cleanup.sh
+++ build-scripts/cleanup.sh
@@ -7,5 +7,3 @@ rm -rv /build-scripts/
 
 pacman -Rs --noconfirm devtools gcc
 pacman -Scc --noconfirm
-
-rm -r /usr/share/{locale,man,doc}
--- images/setup-photoview.sh
+++ images/setup-photoview.sh
@@ -2,14 +2,19 @@
 
 set -eu
 
+ln -s /usr/bin/vendor_perl/exiftool /usr/bin/exiftool
+
 cat >/etc/photoview.env <<EOF
 PHOTOVIEW_DATABASE_DRIVER=sqlite
 PHOTOVIEW_SQLITE_PATH=/var/lib/photoview/photoview.db
+PHOTOVIEW_MEDIA_CACHE=/var/cache/photoview/media_cache
 
 PHOTOVIEW_LISTEN_IP=localhost
 PHOTOVIEW_LISTEN_PORT=8000
 
 PHOTOVIEW_SERVE_UI=1
+PHOTOVIEW_UI_PATH=/usr/share/webapps/photoview-ui
+
 PHOTOVIEW_DEVELOPMENT_MODE=0
 EOF
 
