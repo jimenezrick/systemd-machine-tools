499fd943822129e19e1727f65dbc72e0265f1416
Ricardo Catalinas Jiménez <r@untroubled.be>
2023-12-15 01:31:50 +0000

    Tweak home-gallery config and generate dir

--- images/home-gallery/setup-gallery-web.sh
+++ images/home-gallery/setup-gallery-web.sh
@@ -7,7 +7,7 @@ install --directory --owner=user --group=user /home/user/{.config/home-gallery,.
 install --mode=644 --owner=user --group=user /build/gallery.config.yml /home/user/.config/home-gallery/gallery.config.yml
 
 sudo -u user gallery run import
-sudo -u user -i gallery export static -d .config/home-gallery/database.db -s .cache/home-gallery/storage -f /web/gallery.tar.gz
+sudo -u user -i gallery export static -d .config/home-gallery/database.db -s .cache/home-gallery/storage -o /web/gallery
 
 # For debugging:
 #bash
