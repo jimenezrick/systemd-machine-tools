03c0cd149db54333bb6e610a173476fde448f2ad
Ricardo Catalinas Jiménez <r@untroubled.be>
2023-12-20 09:57:43 +0000

    nit

--- images/home-gallery/setup-gallery-web.sh
+++ images/home-gallery/setup-gallery-web.sh
@@ -6,8 +6,8 @@ install /build/gallery /usr/bin
 install --directory --owner=user --group=user /home/user/{.config/home-gallery,.cache/home-gallery/storage}
 install --mode=644 --owner=user --group=user /build/gallery.config.yml /home/user/.config/home-gallery/gallery.config.yml
 
+# For debugging:
+bash
+
 sudo -u user gallery run import
 sudo -u user -i gallery export static -d .config/home-gallery/database.db -s .cache/home-gallery/storage -o /web/gallery
-
-# For debugging:
-#bash
